4b6f0ec81269fc7b7548c7daad8d3e6dab2c31b1
Alex <46804941+Fr0syaGod@users.noreply.github.com>
2019-04-09 21:14:52 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,10 +1,16 @@
 {
   "name": "autobump",
   "version": "1.0.0",
-  "description": "hi baby",
+  "description": "hi, baby",
   "main": "autobump.js",
   "dependencies": {
-    "discord.js": "11.4.2",
-    "discord.js-commando": "0.10.0",
+    "discord.js": "^11.4.2",
+    "discord.js-commando": "^0.10.0"
+  },
+  "devDependencies": {},
+  "scripts": {
+    "start": "node autobump.js"
+  },
+  "author": "Frosya",
+  "license": "ISC"
 }
-
