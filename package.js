b28475eabbe5f29f0c52a7c6ff775a90d8afb0d4
Alex <46804941+Fr0syaGod@users.noreply.github.com>
2019-04-09 21:04:15 +0300

    Add files via upload

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "autobump",
+  "description": "auto bump server Fr0sya",
+  "main": "autobump.js",
+  "scripts": {
+    "start": "node autobump.js"
+	},
+  "dependencies": {
+   "discord.js": "11.4.2"?
+   "request": "2.81.0"
+   }
+  }
